b53c4d1564ab53652d39eeba241f1973e50f7654
dsmalta <davidwaffle30@gmail.com>
2026-04-20 14:40:36 -0300

    feat: create listar_missoes listar_missoes_completas completar_missoes functions

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,11 @@
-def registrar_conta(dict_contas: dict, input_usuario, input_senha) -> bool:
+def registrar_conta(dict_contas: dict, input_usuario: str, input_senha: str) -> bool:
     if dict_contas.get(input_usuario):
         print("Usuario já cadastrado.")
         return False
     dict_contas[input_usuario] = input_senha
 
 
-def fazer_login(dict_contas: dict, input_usuario, input_senha):
+def fazer_login(dict_contas: dict, input_usuario: str, input_senha: str):
     for usuario, senha in dict_contas.items():
         print(usuario, senha)
         if usuario == input_usuario and senha == input_senha:
@@ -13,7 +13,23 @@ def fazer_login(dict_contas: dict, input_usuario, input_senha):
         else:
             print("Erro ao logar")
         
+def listar_missoes(missoes: list) -> None:
+    print("Essas sao as missoes disponiveis:")
+    for i, missao in enumerate(missoes, start = 1):
+        print(f"{i} - {missao["nome"]}")
+        
+def listar_missoes_completas(missoes_completas: list) -> None:
+    print("Essas sao as missoes completas:")
+    for i, missao_completa in enumerate(missoes_completas, start = 1):
+        print(f"{i} - {missao_completa["nome"]}")
 
+def completar_missoes(escolha: int, missoes: list, missoes_completas: list) -> list:
+    indice_real = escolha - 1
+    
+    if 0 <= indice_real < len(missoes):  
+        missao = missoes.pop(indice_real)
+        missoes_completas.append(missao)
+    return missoes, missoes_completas
 
 if __name__ == "__main__":
     dict_logins = {"Artur": "1234"}
--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def limpar():
 
 def exibir_menu():
     dict_logins = {}
+    missoes = [{"nome" : "Dragao"}, {"nome" : "Caverna"}]
+    missoes_completas = []
     
     rodando = True
     while rodando:
         print("--- MENU CARE PLUS ---\n")
         print("1 - Criar conta")
         print("2 - Fazer login")
+        print("3 - Listar missoes")
+        print("4 - Completar missao")
         print("0 - Sair")
 
         input_escolha = input("Opção: ")
@@ -25,21 +29,34 @@ def exibir_menu():
                 
                 registrar_conta(dict_logins, nome, senha)
                 
-                input("\nConta registrada! Enter para voltar...")
+                input("\nConta registrada! Enter para voltar... ")
                 limpar()
 
             case "2":
                 # Terminar essa aqui
                 print("Ola")
                 fazer_login()
-
+                
+            case "3":
+                limpar()
+                listar_missoes(missoes)
+                listar_missoes_completas(missoes_completas)
+                
+            case "4":
+                limpar()
+                listar_missoes(missoes)
+                completa_missao = int(input("Digite um numero para completar uma missao: "))
+                completar_missoes(completa_missao, missoes, missoes_completas)
+                input("\nMissao Completa! Enter para voltar... ")
+                limpar()
+                
             case "0":
                 print("Saindo do sistema...")
                 rodando = False
             
             case _:
                 print("Opção inválida!")
-                input("Pressione Enter para continuar...")
+                input("Pressione Enter para continuar... ")
                 limpar()
 
 limpar()
