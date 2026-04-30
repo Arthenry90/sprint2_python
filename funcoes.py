f3a03a53295d533080d0912eadeb4a1ef529bc9d
dsmalta <davidwaffle30@gmail.com>
2026-04-30 01:34:48 -0300

    chore: adicionar comentarios ao codigo

--- funcoes.py
+++ funcoes.py
@@ -1,43 +1,35 @@
 def registrar_conta(dict_contas: dict, input_usuario: str, input_senha: str) -> bool:
+    # Registra um novo usuário no sistema
     if dict_contas.get(input_usuario):
-        print("Usuario já cadastrado.")
+        print("Usuário já cadastrado.")
         return False
     dict_contas[input_usuario] = input_senha
 
-
-def fazer_login(dict_contas: dict, input_usuario: str, input_senha: str):
-    for usuario, senha in dict_contas.items():
-        print(usuario, senha)
-        if usuario == input_usuario and senha == input_senha:
-            print("Conta logada com sucesso!")
-        else:
-            print("Erro ao logar! Senha ou úsuario invalídos")
+def fazer_login(dict_contas: dict, input_usuario: str, input_senha: str) -> None:
+    # Checa se o usuário está no dicionário de contas. Caso não esteja, mostra erro
+    if input_usuario in dict_contas and dict_contas[input_usuario] == input_senha:
+        input("\nConta logada com sucesso! Enter para voltar... ")
+    else:
+        input("\nUsuário ou senha inválidos. Enter para voltar... ")
         
 def listar_missoes(missoes: list) -> None:
-    print("Essas sao as missoes disponiveis:")
+    # Lista as missões disponíveis para o usuário completar
+    print("Essas são as missões disponíveis:")
     for i, missao in enumerate(missoes, start = 1):
         print(f"{i} - {missao["nome"]}")
         
 def listar_missoes_completas(missoes_completas: list) -> None:
-    print("Essas sao as missoes completas:")
+    # Lista as missões já completas pelo usuário
+    print("Essas são as missões completas:")
     for i, missao_completa in enumerate(missoes_completas, start = 1):
+        # A lista começa em 1 para o usuário, mas o índice interno continua começando em 0
         print(f"{i} - {missao_completa["nome"]}")
 
 def completar_missoes(escolha: int, missoes: list, missoes_completas: list) -> list:
+    # Move uma missão da lista de disponíveis para a lista de completas
     indice_real = escolha - 1
     
     if 0 <= indice_real < len(missoes):  
         missao = missoes.pop(indice_real)
         missoes_completas.append(missao)
     return missoes, missoes_completas
-
-if __name__ == "__main__":
-    dict_logins = {"Artur": "1234"}
-    # registrar_conta(dict_logins,"artur", "1234")
-
-    fazer_login(dict_logins,"Artur", "1234")
-    # for items in dict_logins.items():
-    #     print(items)
-
-    # for i in dict_logins.items():
-    #     print(i)
--- main.py
+++ main.py
@@ -5,17 +5,34 @@ def limpar():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def exibir_menu():
-    dict_logins = {}
-    missoes = [{"nome" : "Dragao"}, {"nome" : "Caverna"}]
+    
+    # Define um dicionário de contas que armazena usuário e senha
+    dict_contas = {}
+    
+    # Lista de missões disponíveis
+    missoes = [{"nome": "Tomar 1 litro de água"}, 
+               {"nome": "Dar 10.000 passos"},
+               {"nome": "Correr por 20 minutos"},]
+    
+    # Lista de missões completas
     missoes_completas = []
     
     rodando = True
+    # Loop do menu
     while rodando:
-        print("--- MENU CARE PLUS ---\n")
+        print("""
+  __  __                     _____                 _____  _        
+ |  \/  |                   / ____|               |  __ \| |          
+ | \  / | ___ _ __  _   _  | |     __ _ _ __ ___  | |__) | |_   _ ___ 
+ | |\/| |/ _ \ '_ \| | | | | |    / _` | '__/ _ \ |  ___/| | | | / __|
+ | |  | |  __/ | | | |_| | | |___| (_| | | |  __/ | |    | | |_| \__ \\
+ |_|  |_|\___|_| |_|\__,_|  \_____\__,_|_|  \___| |_|    |_|\__,_|___/
+              """)
+        print("======================================================================\n")
         print("1 - Criar conta")
         print("2 - Fazer login")
-        print("3 - Listar missoes")
-        print("4 - Completar missao")
+        print("3 - Listar missões")
+        print("4 - Completar missão")
         print("0 - Sair")
 
         input_escolha = input("Opção: ")
@@ -24,34 +41,39 @@ def exibir_menu():
             case "1":
                 limpar()
                 print("--- CADASTRO DE CONTA")
-                nome = input("Usuário: ")
-                senha = input("Senha: ")
+                nome = input("Usuário.....: ")
+                senha = input("Senha.......: ")
                 
-                registrar_conta(dict_logins, nome, senha)
+                registrar_conta(dict_contas, nome, senha)
                 
                 input("\nConta registrada! Enter para voltar... ")
                 limpar()
 
             case "2":
+                limpar()
                 print("--- LOGAR NA CONTA")
-
-                login_nome = input("Usuário: ")
-                login_senha = input("Senha: ")
-                fazer_login(dict_logins,login_nome,login_senha)
-                input("\nConta logada! Enter para voltar... ")
-
+                login_nome = input("Usuário.....: ")
+                login_senha = input("Senha.......: ")
                 
+                fazer_login(dict_contas, login_nome, login_senha)
+                
+                limpar()
+
             case "3":
                 limpar()
+                print("--- MISSÕES DISPONÍVEIS")
                 listar_missoes(missoes)
+                print("\n--- MISSÕES COMPLETAS")
                 listar_missoes_completas(missoes_completas)
+                input("\nEnter para voltar... ")
+                limpar()
                 
             case "4":
                 limpar()
                 listar_missoes(missoes)
-                completa_missao = int(input("Digite um numero para completar uma missao: "))
+                completa_missao = int(input("\nDigite um número para completar uma missão: "))
                 completar_missoes(completa_missao, missoes, missoes_completas)
-                input("\nMissao Completa! Enter para voltar... ")
+                input("\nMissão completa! Enter para voltar... ")
                 limpar()
                 
             case "0":
@@ -59,7 +81,7 @@ def exibir_menu():
                 rodando = False
             
             case _:
-                print("Opção inválida!")
+                print("\nOpção inválida!")
                 input("Pressione Enter para continuar... ")
                 limpar()
 
